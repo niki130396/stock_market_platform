50adfbc23c6eec93f901c3b9339a6b0e67626016
nikolay.petrov <niki130396@gmail.com>
2021-03-20 15:28:56 +0200

    Create model for financial statements meta data

--- stock_market_platform/financial_statements/models.py
+++ stock_market_platform/financial_statements/models.py
@@ -3,9 +3,13 @@ from djongo import models
 # Create your models here.
 
 
-class SomeTable(models.Model):
-    """
-    Test pre-commit
-    """
-
-    some_field = models.CharField(max_length=120)
+class StatementsMetaData(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    symbol = models.CharField(max_length=15, unique=True)
+    name = models.CharField(max_length=255)
+    market_cap = models.BigIntegerField(blank=True, null=True)
+    country = models.CharField(max_length=30, blank=True, null=True)
+    ipo_year = models.IntegerField(blank=True, null=True)
+    volume = models.IntegerField(blank=True, null=True)
+    sector = models.CharField(max_length=255, blank=True, null=True)
+    industry = models.CharField(max_length=255, blank=True, null=True)
