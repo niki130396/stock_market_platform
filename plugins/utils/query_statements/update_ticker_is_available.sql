35e1a95725268c56cea0ee514a5a0b32a5b6e644
nikolay <niki130396@gmail.com>
2021-12-25 10:29:03 +0200

    update sql files

--- plugins/utils/query_statements/update_ticker_is_available.sql
+++ plugins/utils/query_statements/update_ticker_is_available.sql
@@ -1,6 +1,11 @@
-UPDATE "public".financial_statements_statementsmetadata
-SET is_available = true, is_processed = false
-WHERE symbol = '{{ symbol }}'
-AND is_income_statement_available = true
-AND is_balance_sheet_statement_available = true
-AND is_cash_flow_statement_available = true;
+UPDATE "public".financial_statements_statementsmetadata meta
+SET is_available =
+    CASE
+        WHEN meta.is_income_statement_available = true
+            AND meta.is_balance_sheet_statement_available = true
+            AND meta.is_cash_flow_statement_available = true
+            THEN true
+        ELSE false
+    END,
+    is_processed = false
+WHERE meta.symbol = '{{ symbol }}';
