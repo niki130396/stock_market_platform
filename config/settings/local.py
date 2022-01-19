177386a9664371f2738f3b7cf8a1b8a9ee14b6f5
nikolay <niki130396@gmail.com>
2022-01-19 20:07:30 +0200

    Add GCP external address as allowed host

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="QO0cH0nX4JwvaaZ8pFFoPrIocvAZchBvcHC3QQrDUsfAT2NOi33CJkgwV3zThz9D",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "35.195.40.21"]
 
 # CACHES
 # ------------------------------------------------------------------------------
