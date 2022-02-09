5af6b036cc6485dc585eeefd5cab9159462e92d8
nikolay <niki130396@gmail.com>
2022-02-09 12:21:20 +0200

    Remove selenium middleware

--- scrapy_tasks/scrapy_tasks/middlewares.py
+++ scrapy_tasks/scrapy_tasks/middlewares.py
@@ -6,11 +6,6 @@
 
 # useful for handling different item types with a single interface
 from scrapy import signals
-from scrapy.http import HtmlResponse
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-
-from .requests import SeleniumRequest
 
 
 class ScrapyTasksSpiderMiddleware:
@@ -105,83 +100,3 @@ class ScrapyTasksDownloaderMiddleware:
 
     def spider_opened(self, spider):
         spider.logger.info("Spider opened: %s" % spider.name)
-
-
-class SeleniumMiddleware:
-    """Scrapy middleware handling the requests using selenium"""
-
-    def __init__(self, command_executor, driver_arguments):
-        """Initialize the selenium webdriver
-        Parameters
-        ----------
-        driver_name: str
-            The selenium ``WebDriver`` to use
-        driver_executable_path: str
-            The path of the executable binary of the driver
-        driver_arguments: list
-            A list of arguments to initialize the driver
-        browser_executable_path: str
-            The path of the executable binary of the browser
-        command_executor: str
-            Selenium remote server endpoint
-        """
-        options = webdriver.ChromeOptions()
-
-        for argument in driver_arguments:
-            options.add_argument(argument)
-
-        if command_executor is not None:
-            self.driver = webdriver.Remote(
-                command_executor=command_executor, options=options
-            )
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        """Initialize the middleware with the crawler settings"""
-
-        command_executor = "http://stock_market_selenium:4444/wd/hub"
-        driver_arguments = ["--headless", "--no-sandbox"]
-
-        middleware = cls(
-            command_executor=command_executor, driver_arguments=driver_arguments
-        )
-
-        crawler.signals.connect(middleware.spider_closed, signals.spider_closed)
-
-        return middleware
-
-    def process_request(self, request, spider):
-        """Process a request using the selenium driver if applicable"""
-
-        if not isinstance(request, SeleniumRequest):
-            return None
-
-        self.driver.get(request.url)
-        spider.logger.warning("HERE IS THE URL!!!!!!!!!!!!!!!!!!!!!!!!")
-        spider.logger.warning(str(request.url))
-
-        for cookie_name, cookie_value in request.cookies.items():
-            self.driver.add_cookie({"name": cookie_name, "value": cookie_value})
-
-        if request.wait_until:
-            WebDriverWait(self.driver, request.wait_time).until(request.wait_until)
-
-        if request.screenshot:
-            request.meta["screenshot"] = self.driver.get_screenshot_as_png()
-
-        # if request.script and element_exists(self.driver, By.CSS_SELECTOR, "button.btn:nth-child(5)"):
-        #     self.driver.execute_script(request.script)
-
-        body = str.encode(self.driver.page_source)
-
-        # Expose the driver via the "meta" attribute
-        request.meta.update({"driver": self.driver})
-
-        return HtmlResponse(
-            self.driver.current_url, body=body, encoding="utf-8", request=request
-        )
-
-    def spider_closed(self):
-        """Shutdown the driver when spider is closed"""
-
-        self.driver.quit()
