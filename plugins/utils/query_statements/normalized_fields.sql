1fd5b506ac24029844a9f98ce5d549392a8bbc6e
nikolay <niki130396@gmail.com>
2022-02-15 16:51:43 +0200

    Correct table name

--- plugins/utils/query_statements/normalized_fields.sql
+++ plugins/utils/query_statements/normalized_fields.sql
@@ -2,7 +2,7 @@ SELECT line.name,
        normalized.name,
        normalized.statement_type
 FROM crawling_financialstatementline line
-JOIN crawling_normalizedfield normalized
+JOIN crawling_normalizedfieldtree normalized
 ON line.normalized_field_id = normalized.field_id
 WHERE crawling_source_id IN (SELECT details.crawling_source_id FROM crawling_crawlingsourcedetails details
                              WHERE details.name = '{{ source_name }}')
