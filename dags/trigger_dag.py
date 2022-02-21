c96a9c70ea498ee6f4dee4817dac925c8314b606
nikolay <niki130396@gmail.com>
2022-02-21 13:22:47 +0200

    Re-trigger stock_analysis dag 10 minutes after previous finish

--- dags/trigger_dag.py
+++ dags/trigger_dag.py
@@ -5,13 +5,13 @@ from airflow.utils.dates import days_ago
 with DAG(
     "trigger_dag",
     start_date=days_ago(1),
-    schedule_interval="*/2 * * * *",
+    schedule_interval="*/10 * * * *",
     max_active_runs=1,
 ) as trigger_dag:
 
     trigger_test_dag = TriggerDagRunOperator(
         task_id="trigger_test_dag",
-        trigger_dag_id="test_dag",
+        trigger_dag_id="stock_analysis_dag",
         allowed_states=["success", "failed"],
         wait_for_completion=True,
     )
