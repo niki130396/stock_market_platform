441ed6400ceb07ef6f74a68c6122ff6eb06aecb1
nikolay <niki130396@gmail.com>
2022-02-21 14:55:33 +0200

    Add sleep and retrigger tasks

--- dags/stock_analysis_dag.py
+++ dags/stock_analysis_dag.py
@@ -2,6 +2,7 @@ from os import environ
 
 from airflow.models import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.utils.dates import days_ago
 
 airflow_home = environ["AIRFLOW_HOME"]
@@ -13,8 +14,18 @@ with DAG(
     max_active_runs=1,
 ) as stock_analysis_dag:
 
-    crawl_yahoo_finance = BashOperator(
+    crawl_stock_analysis_dot_com = BashOperator(
         task_id="crawl",
         bash_command="cd ${AIRFLOW_HOME}/scrapy_tasks &&"
         " scrapy crawl stock_analysis_spider",
     )
+
+    sleep_for_ten_minutes = BashOperator(task_id="sleep", bash_command="sleep 600")
+
+    retrigger_dag = TriggerDagRunOperator(
+        task_id="retrigger_dag",
+        trigger_dag_id="stock_analysis_dag",
+        allowed_states=["success", "failed"],
+    )
+
+    crawl_stock_analysis_dot_com >> sleep_for_ten_minutes >> retrigger_dag
